44f9bc9ff8279d9495650ee342cd3fa42def8ac8
jemy <kndjemy@gmail.com>
2020-02-26 21:38:46 +0300

    first

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+export default class Mine extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <h1>Helooo try this</h1>
+      </div>
+    )
+  }
+}
